8d0d981fc9b8f45298fbc2f699404324ad34bc0c
QIWEI GAO <oohmyv@gmail.com>
2017-03-13 18:14:47 +0800

    commonFunctions.js

---
+++ js/commonFunctions.js
@@ -0,0 +1,8 @@
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+		function( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+			return window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+
