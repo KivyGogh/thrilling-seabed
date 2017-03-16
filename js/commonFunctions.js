daf2a7e1009383fb399e7f333c9cf15bac601c18
QIWEI GAO <oohmyv@gmail.com>
2017-03-16 22:40:32 +0800

    update

--- js/commonFunctions.js
+++ js/commonFunctions.js
@@ -10,3 +10,15 @@ function calLength2(x1, y1, x2, y2) {
 	return Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2);
 }
 
+function randomColor() {
+	var col = [0, 1, 2];
+	col[0] = Math.random() * 100 + 155;
+	col[0] = col[0].toFixed();
+	col[1] = Math.random() * 100 + 155;
+	col[1] = col[1].toFixed();
+	col[2] = Math.random() * 100 + 155;
+	col[2] = col[2].toFixed();
+	var num = Math.floor(Math.random() * 3);
+	col[num] = 0;
+	return "rgba(" + col[0] + "," + col[1] + "," + col[2] + ",";
+}
