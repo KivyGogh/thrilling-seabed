aafbe920bf7158fa088c78e758b1c01b27a3a248
QIWEI GAO <oohmyv@gmail.com>
2017-03-18 21:18:34 +0800

    update

--- js/commonFunctions.js
+++ js/commonFunctions.js
@@ -31,3 +31,10 @@ function lerpDistance(aim, cur, ratio) {
 function inOboundary(arrX, arrY, l, r, t, b) { //在l r t b范围内的检测
 	return arrX > l && arrX < r && arrY > t && arrY < b;
 }
+
+function rgbNum(r, g, b) {
+	r = Math.round(r * 256);
+	g = Math.round(g * 256);
+	b = Math.round(b * 256);
+	return "rgba(" + r + "," + g + "," + b;
+}
