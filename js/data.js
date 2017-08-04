27a14e255ef97a025353c9dbedad459d8e37f72f
QIWEI GAO <oohmyv@gmail.com>
2017-08-04 16:01:47 +0800

    update

--- js/data.js
+++ js/data.js
@@ -9,7 +9,26 @@ var dataObj = function()
 
 dataObj.prototype.draw = function()
 {
+	var w = can1.width;
+	var h = can1.height;
 	
+	ctx1.save();
+	ctx1.shadowBlur = 10;
+	ctx1.shadowColor = "white";
+	ctx1.fillStyle = "white";
+//	ctx1.fillText("num " + this.fruitNum, w * 0.5, h - 50);
+//	ctx1.fillText("double " + this.double, w * 0.5, h - 80);
+	ctx1.fillText("海洋战利品: " + this.score, w * 0.5, h - 20);
+	
+	if(this.gameOver)
+	{
+		this.alpha += deltaTime * 0.0005;
+		if(this.alpha > 1)
+		this.alpha = 1;
+		ctx1.fillStyle = "rgba(255,255,255," + this.alpha +")";
+		ctx1.fillText("GAMEOVER", w * 0.5 , h * 0.5);
+	}
+	ctx1.restore();
 }
 dataObj.prototype.addScore = function()
 {
